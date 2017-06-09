083426284938b1969eddf612e21b831575d646c9
Laurens Van Wiele <vanwiele@talon.one>
2017-06-09 10:55:15 +0200

    randomize event type in tests to avoid race conditions in Travis CI

--- test/test_integration_api_live.rb
+++ test/test_integration_api_live.rb
@@ -17,7 +17,8 @@ class TestIntegrationApiLive < LiveApiTest
 
     management_client.update_campaign_status @app["id"], @campaign["id"], "enabled"
 
-    @attribute ||= management_client.create_attribute({ entity: "Event", eventType: "Viewed Page", name: "URL", title: "Page URL", type: "string", description: "The URL of the page that the user has viewed", tags: [], editable: true })
+    @event_type ||= "Viewed Page#{rand(36**3).to_s(36)}"
+    @attribute ||= management_client.create_attribute({ entity: "Event", eventType: @event_type, name: "URL", title: "Page URL", type: "string", description: "The URL of the page that the user has viewed", tags: [], editable: true })
   end
 
   def teardown
@@ -30,7 +31,7 @@ class TestIntegrationApiLive < LiveApiTest
   end
 
   def test_track_event
-    res = integration_client.track_event "a-session", "Viewed Page", { URL: "http://example.com" }
+    res = integration_client.track_event "a-session", @event_type, { URL: "http://example.com" }
     assert res.profile
     assert res.session
     assert_instance_of TalonOne::Integration::Event, res.event
@@ -38,7 +39,7 @@ class TestIntegrationApiLive < LiveApiTest
     assert !res.event.accepted_coupon?, "No coupon -> no acceptCoupon effect"
     assert_equal 1, res.event.effects.length
     assert_equal "setDiscount", res.event.effects[0].function
-    assert_equal "Viewed Page", res.event.type
+    assert_equal @event_type, res.event.type
     assert_equal "a-session", res.event.session_id, "a-session"
     assert_equal({ "URL" => "http://example.com" }, res.event.attributes)
     assert_instance_of BigDecimal, res.session["discounts"]["Free money"]
